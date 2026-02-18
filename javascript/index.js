34981e6ce4fd6e887fb9c10604e161905bad1591
grza-cyber <grzannantuoni@gmail.com>
2026-02-18 01:00:35 +0100

    Added javascript functionalities to the select element

--- javascript/index.js
+++ javascript/index.js
@@ -2,35 +2,38 @@ function displayDateTime() {
     let dateFormat = "MMMM Do YYYY";
     let am = `[<small>] A [</small>]`;
     let timeFormat = `H:mm:ss ${am}`;
-
-    let zurichElement = document.querySelector("#zurich");
-    zurichElement.innerHTML = "Zürich";
-    let zurichDateElement = document.querySelector("#zurich-date");
-    zurichDateElement.innerHTML = moment().tz("Europe/Zurich").format(dateFormat);
-    let zurichTimeElement = document.querySelector("#zurich-time");
-    zurichTimeElement.innerHTML = moment().tz("Europe/Zurich").format(timeFormat);
-
-    let bathElement = document.querySelector("#bath");
-    bathElement.innerHTML = "Bath";
-    let bathDateElement = document.querySelector("#bath-date");
-    bathDateElement.innerHTML = moment().tz("Europe/London").format(dateFormat);
-    let bathTimeElement = document.querySelector("#bath-time");
-    bathTimeElement.innerHTML = moment().tz("Europe/London").format(timeFormat);
-
     let antananarivoElement = document.querySelector("#antananarivo");
-    antananarivoElement.innerHTML = "Antananarivo";
-    let antananarivoDateElement = document.querySelector("#antananarivo-date");
-    antananarivoDateElement.innerHTML = moment().tz("Indian/Antananarivo").format(dateFormat);
-    let antananarivoTimeElement = document.querySelector("#antananarivo-time");
-    antananarivoTimeElement.innerHTML = moment().tz("Indian/Antananarivo").format(timeFormat);
+    if (antananarivoElement) {
+        antananarivoElement.innerHTML = "Antananarivo";
+        let antananarivoDateElement = document.querySelector("#antananarivo-date");
+        antananarivoDateElement.innerHTML = moment().tz("Indian/Antananarivo").format(dateFormat);
+        let antananarivoTimeElement = document.querySelector("#antananarivo-time");
+        antananarivoTimeElement.innerHTML = moment().tz("Indian/Antananarivo").format(timeFormat);
+    }
+}
+setInterval(displayDateTime, 1000);
 
-    let saopauloElement = document.querySelector("#sao-paulo");
-    saopauloElement.innerHTML = "Sao Paulo";
-    let saopauloDateElement = document.querySelector("#sau-paulo-date");
-    saopauloDateElement.innerHTML = moment().tz("America/Sao_Paulo").format(dateFormat);
-    let saopauloTimeElement = document.querySelector("#sau-paulo-time");
-    saopauloTimeElement.innerHTML = moment().tz("America/Sao_Paulo").format(timeFormat);
+
+function updateTheCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace("_", " ").split('/')[1];
+    let dateFormat = "MMMM Do YYYY";
+    let am = `[<small>]A[</small>]`;
+    let timeFormat = `H:mm:ss ${am}`;
+    let cityDate = moment().tz(cityTimeZone).format(dateFormat);
+    let cityTime = moment().tz(cityTimeZone).format(timeFormat);
+   
+    let cityContainerElement = document.querySelector("#city-container");
+    let cityElement =  
+        `<div class="city">
+        <h2>${cityName}</h2>
+        <div class="date">${cityDate}</div>
+        </div>
+        <div class="time">${cityTime}</div>`
+    cityContainerElement.innerHTML = cityElement;
 }
 
-setInterval(displayDateTime, 1000);
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateTheCity);
+
 
