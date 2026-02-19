1bac44b9349abad64fa2106c454157a46c5b5b51
grza-cyber <grzannantuoni@gmail.com>
2026-02-19 01:39:11 +0100

    Updated html, javascript and css

--- javascript/index.js
+++ javascript/index.js
@@ -10,6 +10,22 @@ function displayDateTime() {
         let antananarivoTimeElement = document.querySelector("#antananarivo-time");
         antananarivoTimeElement.innerHTML = moment().tz("Indian/Antananarivo").format(timeFormat);
     }
+    let bathElement = document.querySelector("#bath");
+    if (bathElement) {
+        bathElement.innerHTML = "Bath";
+        let bathDateElement = document.querySelector("#bath-date");
+        bathDateElement.innerHTML = moment().tz("Europe/London").format(dateFormat);
+        let bathTimeElement = document.querySelector("#bath-time");
+        bathTimeElement.innerHTML = moment().tz("Europe/London").format(timeFormat);
+    }
+    let gyeongjuElement = document.querySelector("#gyeongju");
+    if (gyeongjuElement) {
+        gyeongjuElement.innerHTML = "Gyeongju";
+        let gyeongjuDateElement = document.querySelector("#gyeongju-date");
+        gyeongjuDateElement.innerHTML = moment().tz("Asia/Seoul").format(dateFormat);
+        let gyeongjuTimeElement = document.querySelector("#gyeongju-time");
+        gyeongjuTimeElement.innerHTML = moment().tz("Asia/Seoul").format(timeFormat);
+    }
 }
 setInterval(displayDateTime, 1000);
 
@@ -26,14 +42,19 @@ function updateTheCity(event) {
     let cityDate = moment().tz(cityTimeZone).format(dateFormat);
     let cityTime = moment().tz(cityTimeZone).format(timeFormat);
    
-    let cityContainerElement = document.querySelector("#city-container");
+    let cityContainerElement = document.querySelector("#cities");
     let cityElement =  
-        `<div class="city">
-        <h2>${cityName}</h2>
-        <div class="date">${cityDate}</div>
-        </div>
-        <div class="time">${cityTime}</div>`
+        `<div class="city-select">
+            <div class="city">
+                <h2>${cityName}</h2>
+            <div class="date">${cityDate}</div>
+            </div>
+            <div class="time">${cityTime}</div>
+        </div>`
     cityContainerElement.innerHTML = cityElement;
+  
+    let linkElement = document.querySelector("#link");
+    linkElement.innerHTML = `<a href="/">Back to all cities</a>`;
 }
 
 let citySelectElement = document.querySelector("#city");
